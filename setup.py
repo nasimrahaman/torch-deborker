3a694e84bd32fb72e854b4453069f812e65ad953
Nasim Rahaman <nasim.rahaman@tuebingen.mpg.de>
2021-08-28 16:14:35 +0200

    - add setup.py

---
+++ setup.py
@@ -0,0 +1,12 @@
+from distutils.core import setup
+
+setup(
+    name='torch-deborker',
+    version='0.1',
+    packages=['torch_db'],
+    url='',
+    license='MIT',
+    author='Nasim Rahaman',
+    author_email='nasim.rahaman@tuebingen.mpg.de',
+    description='Debork all the things! '
+)
